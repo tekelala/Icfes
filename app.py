06db93f97b908b46d61e49ae4200b36478715109
tekelala <camilo@tekelala.com>
2023-05-04 17:57:38 -0500

    va

--- app.py
+++ app.py
@@ -73,21 +73,19 @@ if st.button('Iniciar') and uploaded_files is not None:
         else:
             st.write(f"No files in category {category}")
 
-        if st.button('Proponer respuesta'):
-            # Propose a response using GPT-3.5
-            if file and st.button('Proponer respuesta'):
-                with open(f'{category}/{file}', 'r') as f:
-                    text = f.read()
-                try:
-                    # Call the function to get the completion
-                    prompt = f"""
-                    Eres un excelente abogado del ICFES respondiendo {category} el texto que debes responder es {text}
+        if st.button('Proponer respuesta') and file is not None:
+        with open(f'{category}/{file}', 'r') as f:
+            text = f.read()
+        try:
+            # Call the function to get the completion
+            prompt = f"""
+            Eres un excelente abogado del ICFES respondiendo {category} el texto que debes responder es {text}
 
-                    """
-                    st.session_state.content = get_completion(prompt)
-                    st.write(f'Proposed response for {file} in category {category}: {st.session_state.content}')
-                except Exception as e:
-                    st.error(f"Error generating response: {e}")
+            """
+            st.session_state.content = get_completion(prompt)
+            st.write(f'Proposed response for {file} in category {category}: {st.session_state.content}')
+        except Exception as e:
+            st.error(f"Error generating response: {e}")
 
 def extract_text_from_pdf(uploaded_file):
     pdf_reader = PyPDF2.PdfFileReader(uploaded_file)
